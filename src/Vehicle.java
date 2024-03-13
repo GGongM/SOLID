987056354c66af68c82a0479f114448b6dfa96ee
hmpark <hongmipark91@gmail.com>
2024-03-13 16:27:38 +0900

    update ignore

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Vehicle {
+class GasStation {
+	public void reFuel(Vehicle vehicle) {
+		vehicle.setRemainingFuel(vehicle.getMaxFuel());
+	}
+}
+
+public class Vehicle {
+
+	private final int maxFuel;
+	private int remainingFuel;
+
+	public Vehicle(final int maxFuel) {
+		this.maxFuel = maxFuel;
+		remainingFuel = maxFuel;
+	}
+
+	public int getMaxFuel() {
+		return maxFuel;
+	}
+
+	public int getRemainingFuel() {
+		return remainingFuel;
+	}
+
+	public void setRemainingFuel(final int remainingFuel) {
+		this.remainingFuel = remainingFuel;
+	}
+
+	public void accelerate() {
+		remainingFuel--;
+	}
 }
